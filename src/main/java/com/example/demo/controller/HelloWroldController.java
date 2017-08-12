6878f54fc73b9c4a7fd34a79d323cc16413bada4
andrew.deng <262103206@qq.com>
2017-08-12 21:37:47 +0800

    Thymeleaf

---
+++ src/main/java/com/example/demo/controller/HelloWroldController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWroldController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello!";
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public ModelAndView save(@ModelAttribute User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user-data");
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+
+}
